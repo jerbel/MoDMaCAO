59f752b039637f1b8723b2024e0ff6630e7498fb
lennart <lennart@lennart-pc.fritz.box>
2019-07-01 10:47:33 +0200

    Added new test case for deploying a component using the BashCMTool class

--- plugins/org.modmacao.cm/src/org/modmacao/cm/bash/test/BashCMToolTest.java
+++ plugins/org.modmacao.cm/src/org/modmacao/cm/bash/test/BashCMToolTest.java
@@ -1,5 +1,7 @@
 package org.modmacao.cm.bash.test;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -8,13 +10,84 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 
+import org.eclipse.cmf.occi.core.AttributeState;
+import org.eclipse.cmf.occi.core.Mixin;
+import org.eclipse.cmf.occi.core.MixinBase;
+import org.eclipse.cmf.occi.core.OCCIFactory;
+import org.eclipse.cmf.occi.infrastructure.Compute;
+import org.eclipse.cmf.occi.infrastructure.InfrastructureFactory;
+import org.eclipse.cmf.occi.infrastructure.Ipnetworkinterface;
+import org.eclipse.cmf.occi.infrastructure.Networkinterface;
 import org.hamcrest.core.Is;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.modmacao.ansibleconfiguration.AnsibleconfigurationFactory;
+import org.modmacao.ansibleconfiguration.Ansibleendpoint;
+import org.modmacao.cm.ansible.AnsibleCMTool;
+import org.modmacao.cm.bash.BashCMTool;
+import org.modmacao.occi.platform.Application;
+import org.modmacao.occi.platform.Component;
+import org.modmacao.occi.platform.impl.PlatformFactoryImpl;
+import org.modmacao.placement.PlacementFactory;
+import org.modmacao.placement.Placementlink;
 
 import com.jcraft.jsch.*;
 
+import modmacao.impl.ModmacaoFactoryImpl;
+
 public class BashCMToolTest {
+	
+	BashCMTool cmtool = new BashCMTool();
+	Component cut;
+	Application aut;
+	
+	
+	@Before
+	public void setUP() {
+		// Create component
+		cut = new PlatformFactoryImpl().createComponent();
+		
+		cut.setTitle("shard1");
+		MixinBase modmacaoComponentMixinBase = new ModmacaoFactoryImpl().createComponent();
+		Mixin mixin = OCCIFactory.eINSTANCE.createMixin();
+		mixin.setScheme("http://schemas.modmacao.org/modmacao#");
+		mixin.setName("example_component");
+		modmacaoComponentMixinBase.setMixin(mixin);
+				
+		cut.getParts().add(modmacaoComponentMixinBase);
+
+		
+		// create compute 
+		Compute vm1 = InfrastructureFactory.eINSTANCE.createCompute();
+		vm1.setTitle("vm1");
+			
+		Networkinterface nic = InfrastructureFactory.eINSTANCE.createNetworkinterface();
+		Ipnetworkinterface ipNetworkMixinBase = InfrastructureFactory.eINSTANCE.createIpnetworkinterface();
+	
+		Ansibleendpoint ansibleendpoint = AnsibleconfigurationFactory.eINSTANCE.createAnsibleendpoint();
+		
+		AttributeState ipaddress = OCCIFactory.eINSTANCE.createAttributeState();
+		ipaddress.setName("occi.networkinterface.address");
+		// we set ip both as AttributeState and member variable, since otherwise we might encounter inconsistencies
+		ipaddress.setValue("localhost");
+		ipNetworkMixinBase.setOcciNetworkinterfaceAddress("localhost");
+		ipNetworkMixinBase.getAttributes().add(ipaddress);
+		nic.getParts().add(ipNetworkMixinBase);
+		nic.getParts().add(ansibleendpoint);
+		
+		vm1.getLinks().add(nic);
+		
+		// set link between vm and component
+		Placementlink link = PlacementFactory.eINSTANCE.createPlacementlink();
+		link.setTarget(vm1);
+		cut.getLinks().add(link);	
+	}
+	
+	@Test
+	public void testComponentDeploy() {
+		assertEquals(0, cmtool.deploy(cut));
+	}
 	
 //	@Test
 	public void testSSHConnection() {
@@ -86,7 +159,7 @@ public class BashCMToolTest {
 		}
 	}
 	
-	@Test
+//	@Test
 	public void testJsch() {
 		System.out.println("testJsch:");
 		try {
