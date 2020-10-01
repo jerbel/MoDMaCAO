be25e62d76c584e1d1dac1a7656c86b3dc916678
erbel <johannes.erbel@cs.uni-goettingen.de>
2020-10-01 14:48:56 +0200

    Add Network connection

--- plugins/org.modmacao.openstack.connector/src-gen/org/modmacao/openstack/connector/PostConstructor.java
+++ plugins/org.modmacao.openstack.connector/src-gen/org/modmacao/openstack/connector/PostConstructor.java
@@ -14,6 +14,7 @@ package org.modmacao.openstack.connector;
 import org.openstack4j.api.OSClient;
 import org.openstack4j.model.compute.Server;
 import org.openstack4j.model.compute.Server.Status;
+import org.openstack4j.model.network.Network;
 import org.openstack4j.openstack.OSFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,6 +36,7 @@ import org.eclipse.cmf.occi.core.Resource;
 import org.eclipse.cmf.occi.infrastructure.Compute;
 import org.eclipse.cmf.occi.infrastructure.ComputeStatus;
 import org.eclipse.cmf.occi.infrastructure.InfrastructureFactory;
+import org.eclipse.cmf.occi.infrastructure.NetworkStatus;
 import org.occiware.mart.server.exception.ConfigurationException;
 import org.occiware.mart.server.model.ConfigurationManager;
 import org.occiware.mart.server.model.EntityManager;
@@ -78,38 +80,16 @@ public class PostConstructor implements Runnable {
 				
 				
 				List<? extends Server> sList = os.compute().servers().list();
+				addMissingServer(sList);
+				removeNonExistentComputeNodes(sList);
 				
-				for(Server s: sList) {
-					if(isInRuntimeModel(s) == false) {
-						LOGGER.info("Server missing in runtimer model: "+ s);
-						addServerToRuntimeModel(s);
-					}
-				}
+				List<? extends Network> nList = os.networking().network().list();
+				addMissingNetworks(nList);
+				removeNonExistentNetworkNodes(nList);
 				
-				List<String> toRemove = new ArrayList<String>();
-				Configuration config = ConfigurationManager.getConfigurationForOwner("anonymous");
-				for(Resource res: config.getResources()) {
-					if(res instanceof Compute) {
-						for(MixinBase mixB: res.getParts()) {
-							if(mixB instanceof Runtimeid) {
-								Runtimeid rid = (Runtimeid) mixB;
-								if(ridExistsInCloud(rid, sList) == false) {
-									toRemove.add(res.getLocation());
-								}
-							}
-						}
-					}
-				}
+				//addMissingNetworkinterfaces(sList);
+				//removeNonExistentNetworkinterfaces(sList);
 				
-				for(String str: toRemove) {
-					try {
-						LOGGER.info("Removing: " + str);
-						EntityManager.removeOrDissociateFromConfiguration(str, "anonymous");
-					} catch (ConfigurationException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
 				
 				//LOGGER.info("Network: " + os.networking().network().list());
 			}
@@ -117,9 +97,143 @@ public class PostConstructor implements Runnable {
 		running = false;
 	}
 
-	private boolean ridExistsInCloud(Runtimeid rid, List<? extends Server> sList) {
+	private void removeNonExistentNetworkNodes(List<? extends Network> nList) {
+		List<String> toRemove = new ArrayList<String>();
+		Configuration config = ConfigurationManager.getConfigurationForOwner("anonymous");
+		for(Resource res: config.getResources()) {
+			if(res instanceof Compute) {
+				for(MixinBase mixB: res.getParts()) {
+					if(mixB instanceof Runtimeid) {
+						Runtimeid rid = (Runtimeid) mixB;
+						if(networkidExistsInCloud(rid, nList) == false) {
+							toRemove.add(res.getLocation());
+						}
+					}
+				}
+			}
+		}
+		
+		for(String str: toRemove) {
+			try {
+				LOGGER.info("Removing: " + str);
+				EntityManager.removeOrDissociateFromConfiguration(str, "anonymous");
+			} catch (ConfigurationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+	private void addMissingNetworks(List<? extends Network> nList) {
+		for(Network n: nList) {
+			if(isInRuntimeModel(n.getId()) == false) {
+				LOGGER.info("Adding missing Network to runtime model: "+ n);
+				addNetworkToRuntimeModel(n);
+			}
+		}
+		
+	}
+
+	private void addNetworkToRuntimeModel(Network n) {
+		OCCIFactory cfac = OCCIFactory.eINSTANCE;
+		InfrastructureFactory fac = InfrastructureFactory.eINSTANCE;
+		org.eclipse.cmf.occi.infrastructure.Network nw = fac.createNetwork();
+		String title = n.getName();
+		AttributeState state = cfac.createAttributeState();
+		state.setName("occi.network.state");
+		if(n.isAdminStateUp()) {
+			nw.setOcciNetworkState(NetworkStatus.ACTIVE);
+			state.setValue("active");
+		} else {
+			nw.setOcciNetworkState(NetworkStatus.INACTIVE);
+			state.setValue("inactive");
+		}
+		
+		
+		
+		
+		
+		String uuid = nw.getId();
+		String kind = nw.getKind().getScheme() + nw.getKind().getTerm();
+		String summary = "Created by postconstructor!";
+		
+		OpenstackruntimeFactory oFac = OpenstackruntimeFactory.eINSTANCE;
+		Runtimeid rid = oFac.createRuntimeid();
+		rid.setOpenstackRuntimeId(nw.getId());
+		AttributeState as = cfac.createAttributeState();
+		as.setName("openstack.runtime.id");
+		as.setValue(nw.getId());
+		rid.getAttributes().add(as);
+		
+		nw.getParts().add(rid);
+		
+		List<String> mixins = getMixinNames(nw);
+		Map<String, String> attributes = getAttributeMap(nw);
+		
+		
+		String location = "/" + nw.getKind().getTerm() + "/urn:uuid:" + uuid + "/";
+		String owner = "anonymous";
+
+		try {
+			EntityManager.addResourceToConfiguration(uuid, title, summary, kind, mixins, attributes, location, owner);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	private void removeNonExistentComputeNodes(List<? extends Server> sList) {
+		List<String> toRemove = new ArrayList<String>();
+		Configuration config = ConfigurationManager.getConfigurationForOwner("anonymous");
+		for(Resource res: config.getResources()) {
+			if(res instanceof Compute) {
+				for(MixinBase mixB: res.getParts()) {
+					if(mixB instanceof Runtimeid) {
+						Runtimeid rid = (Runtimeid) mixB;
+						if(serveridExistsInCloud(rid, sList) == false) {
+							toRemove.add(res.getLocation());
+						}
+					}
+				}
+			}
+		}
+		
+		for(String str: toRemove) {
+			try {
+				LOGGER.info("Removing: " + str);
+				EntityManager.removeOrDissociateFromConfiguration(str, "anonymous");
+			} catch (ConfigurationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private void addMissingServer(List<? extends Server> sList) {
+		for(Server s: sList) {
+			if(isInRuntimeModel(s.getId()) == false) {
+				//Dont add resource to model while connector is still running
+				if(s.getStatus().equals(Status.BUILD) == false) {				
+					LOGGER.info("Add missing Server to runtime model: "+ s);
+					addServerToRuntimeModel(s);
+				}
+			}
+		}
+	}
+
+	private boolean serveridExistsInCloud(Runtimeid rid, List<? extends Server> sList) {
 		for(Server s: sList) {
 			if(rid.getOpenstackRuntimeId().equals(s.getId())) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private boolean networkidExistsInCloud(Runtimeid rid, List<? extends Network> nList) {
+		for(Network n: nList) {
+			if(rid.getOpenstackRuntimeId().equals(n.getId())) {
 				return true;
 			}
 		}
@@ -173,9 +287,8 @@ public class PostConstructor implements Runnable {
 		}
 	}
 
-	private boolean isInRuntimeModel(Server s) {
-		//Dont add resource to model while connector is still running
-		if(isBlacklisted(s) || s.getStatus().equals(Status.BUILD)) {
+	private boolean isInRuntimeModel(String id) {
+		if(isBlacklisted(id)) {
 			return true;
 		}
 		Configuration config = ConfigurationManager.getConfigurationForOwner("anonymous");
@@ -184,7 +297,7 @@ public class PostConstructor implements Runnable {
 				for(MixinBase mixB: res.getParts()) {
 					if(mixB instanceof Runtimeid) {
 						Runtimeid rid = (Runtimeid) mixB;
-						if(rid.getOpenstackRuntimeId().equals(s.getId())) {
+						if(rid.getOpenstackRuntimeId().equals(id)) {
 							return true;
 						}
 					}
@@ -195,10 +308,12 @@ public class PostConstructor implements Runnable {
 	}
 
 	
-	private boolean isBlacklisted(Server s) {
-		if(s.getId().equals("5a3ba352-9341-4d52-b7f0-96c75f9ceffa")
-				|| s.getId().equals("4d74302f-5cef-4bcd-ab12-5ac2455a68ab")
-				|| s.getId().equals("7603cefe-affb-4765-8a81-be74d9f7aebe")) {
+	private boolean isBlacklisted(String id) {
+		if(id.equals("5a3ba352-9341-4d52-b7f0-96c75f9ceffa")
+				|| id.equals("4d74302f-5cef-4bcd-ab12-5ac2455a68ab")
+				|| id.equals("75a4639e-9ce7-4058-b859-8a711b0e2e7b")
+				|| id.equals("82ef5768-0cac-4f77-920e-c871fbfd7f3f")
+				|| id.equals("7603cefe-affb-4765-8a81-be74d9f7aebe")) {
 				return true;
 		} else {
 			return false;
